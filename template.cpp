9c7151ed00805afda233e8936660b8e0129effdc
Debashis Nandi <debashisnandi576@gmail.com>
2025-07-02 01:54:50 +0530

    core: git init and project setup

---
+++ template.cpp
@@ -0,0 +1,36 @@
+/*
+Problem Id = 
+Tags = 
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void FastIO()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+}
+
+class Solution
+{
+public:
+	void Solve()
+	{
+		
+	}
+};
+
+int main()
+{
+	FastIO();
+
+
+	return 0;
+}
