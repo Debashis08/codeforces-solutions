787e113492635e2449c8ecca05d16557f3ec424d
Debashis Nandi <debashisnandi576@gmail.com>
2026-05-30 01:18:18 +0530

    feat: 1791C solved

---
+++ src/01700-01799/01791C.cpp
@@ -0,0 +1,114 @@
+/*
+Tags
+
+two-pointer
+
+----------------------------------------------------------------------------------------------------------------------------
+
+Problem Description
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <limits>
+#include <iomanip>
+#include <functional>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+// Fast I/O
+void fastIo()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+}
+
+class Solution
+{
+private:
+	long long gcd(long long a, long long b)
+	{
+		while (b)
+		{
+			a %= b;
+			swap(a, b);
+		}
+		return a;
+	}
+
+	long long lcm(long long a, long long b)
+	{
+		return a / this->gcd(a, b) * b;
+	}
+
+	long long power(long long base, long long exp, long long mod = MOD)
+	{
+		long long result = 1;
+		base %= mod;
+		while (exp > 0)
+		{
+			if (exp & 1)
+				result = result * base % mod;
+			base = base * base % mod;
+			exp >>= 1;
+		}
+		return result;
+	}
+
+public:
+	int findShortestPossibleString(string s, int size)
+	{
+		int left = 0;
+		int right = size - 1;
+		while (left <= right)
+		{
+			if (s[left] != s[right])
+			{
+				size -= 2;
+				left++;
+				right--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return size;
+	}
+};
+
+int main()
+{
+	fastIo();
+    int t,n;
+	string s;
+	cin >> t;
+	Solution sol;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n;
+		cin >> s;
+		cout << sol.findShortestPossibleString(s, n) << endl;
+	}
+
+	return 0;
+}
