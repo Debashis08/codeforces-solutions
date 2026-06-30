ca4730a24a4e9ef47765e83b85c092465b31570a
Debashis Nandi <debashisnandi576@gmail.com>
2026-06-30 22:27:59 +0530

    feat: cfr 1107 div 3 solved 2

---
+++ src/02200-02299/02241A.cpp
@@ -0,0 +1,105 @@
+/*
+Tags
+
+
+
+----------------------------------------------------------------------------------------------------------------------------
+
+Problem Description
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <limits>
+#include <iomanip>
+#include <functional>
+#include <cstring>
+#include <climits>
+using namespace std;
+using ll = long long;
+
+const int MOD = 1e9 + 7;
+
+// Fast I/O
+void fastIo()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+}
+
+class Solution
+{
+private:
+	ll gcd(ll a, ll b)
+	{
+		while (b)
+		{
+			a %= b;
+			swap(a, b);
+		}
+		return a;
+	}
+
+	ll lcm(ll a, ll b)
+	{
+		return a / this->gcd(a, b) * b;
+	}
+
+	ll power(ll base, ll exp, ll mod = MOD)
+	{
+		ll result = 1;
+		base %= mod;
+		while (exp > 0)
+		{
+			if (exp & 1)
+				result = result * base % mod;
+			base = base * base % mod;
+			exp >>= 1;
+		}
+		return result;
+	}
+
+public:
+	void solve()
+	{
+		int x, y;
+		cin >> x >> y;
+		if (x % y == 0)
+		{
+			cout << "YES" << "\n";
+		}
+		else
+		{
+			cout << "NO" << "\n";
+		}
+	}
+};
+
+int main()
+{
+	fastIo();
+	int t;
+	Solution sol;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		sol.solve();
+	}
+
+	return 0;
+}
